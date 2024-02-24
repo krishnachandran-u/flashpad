11ce5412abdcc2d3a57831e67f7ca82cce3b28aa
krishnachandran-u <kriss52450@gmail.com>
2024-02-25 02:01:40 +0530

    finish raw mode

--- main.cpp
+++ main.cpp
@@ -2,36 +2,48 @@
 #include <iostream>
 #include <termios.h>
 #include <unistd.h>
+#include <stdbool.h>
+#include <string>
 
 struct termios original_termios;
 
+void kill(const std::string& s) {
+    std::cerr << s << std::endl;
+    exit(1);
+}
+
 void disableRawMode() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_termios); //reset
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_termios) == -1) kill("tcsetattr");
 }
 
 void enableRawMode() {
-    tcgetattr(STDIN_FILENO, &original_termios); 
-
+    if(tcgetattr(STDIN_FILENO, &original_termios) == -1) kill("tcgetattr");
     struct termios raw_termios = original_termios;
     
-    raw_termios.c_lflag &= ~(ECHO | ICANON); 
+    raw_termios.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw_termios.c_iflag &= ~(OPOST);
+    raw_termios.c_iflag &= ~(CS8);
+    raw_termios.c_lflag &= ~(ECHO | ICANON | ISIG); 
+    raw_termios.c_cc[VMIN] = 0;
+    raw_termios.c_cc[VTIME] = 1;
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw_termios); 
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw_termios) == -1) kill("tcsetattr");
 }
 
 int main() {
     enableRawMode();
+    atexit(disableRawMode);
 
-    char c;
-    while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    while(true) {
+        char c = '\0';
+        read(STDIN_FILENO, &c, 1);
         if((iscntrl(c))) {
-            std::cout << c << "\n";
+            std::cout << static_cast<int>(c) << std::endl; //control characters doesnot have character definitions
         }
         else {
-            std::cout << (int)c << " " << c << "\n";
+            std::cout << static_cast<int>(c) << ' ' << "(\'" << c << "\')" << std::endl;
         }
     }
 
-    disableRawMode();
     return 0;
 }
--- makefile
+++ makefile
@@ -1,4 +1,19 @@
 CXX = g++
+CXXVERSION = -std=c++20
+CXXFLAGS = -Wall -Wextra -Wpedantic 
 
+SRC = main.cpp
+TARGET = main
 
+.PHONY: all build run clean
 
+all: build run
+
+build: $(SRC)
+	$(CXX) $(CXXVERSION) $(CXXFLAGS) -o $(TARGET) $(SRC)
+
+run: build
+	./$(TARGET)
+
+clean:
+	rm -rf $(TARGET)
